2742a8aa1a33c655ef6d3c0a4d204765f9dbb83a
Gabriel Leonel <gabrielvista7@gmail.com>
2025-12-10 01:51:09 -0300

    Bootstrap instalado; Pagina loja iniciada

--- elyndra_ui/elyndra/settings.py
+++ elyndra_ui/elyndra/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'loja'
 ]
 
 MIDDLEWARE = [
@@ -72,12 +73,7 @@ WSGI_APPLICATION = 'elyndra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
